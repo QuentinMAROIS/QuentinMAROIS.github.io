fb82469b190fc0e794d87ff03a26a8d217cc2ab6
Quentin Marois <quentinmarois.indp@gmail.com>
2020-09-27 14:03:07 +0200

    Removed one background image

--- script/script_titanic.js
+++ script/script_titanic.js
@@ -24,7 +24,6 @@ function randombg(){
                  "url('src/background1/bg2.jpg')",
                  "url('src/background1/bg3.jpg')",
                  "url('src/background1/bg4.jpg')",
-                 "url('src/background1/bg5.jpg')",
-                 "url('src/background1/bg6.jpg')"];
+                 "url('src/background1/bg5.jpg')"];
   document.getElementById("background").style.backgroundImage=bigSize[random];
 }
